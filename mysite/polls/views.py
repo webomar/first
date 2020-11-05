16c315a119d532239e156567716962deabee1cf0
webomar <omaralubwani@yahoo.com>
2020-11-05 23:48:24 +0100

    late_night

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,3 +6,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_iddd):
+    return HttpResponse("You're looking at question %s." % question_iddd)
+
+def results(request, question_id):
+ #   responseBitch = "You're looking at the results of question %s."
+ #   return HttpResponse(responseBitch % question_id)
+    return HttpResponse("You're looking at the results of question %s." % question_id)
+    
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
